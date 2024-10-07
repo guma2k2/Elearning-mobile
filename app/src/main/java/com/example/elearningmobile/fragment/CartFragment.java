73d2def44f50dbd3fb9a48cc19d2fbe31533e017
Thuan ngo <thuanngo3072002@gmail.com>
2024-10-07 23:15:39 +0700

    add viewmodel

---
+++ app/src/main/java/com/example/elearningmobile/fragment/CartFragment.java
@@ -0,0 +1,37 @@
+package com.example.elearningmobile.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.elearningmobile.R;
+
+
+public class CartFragment extends Fragment {
+
+    private Context context;
+    public CartFragment() {
+    }
+    public CartFragment(Context context) {
+        this.context = context;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+}
